c8c8128cedf4400a23f0eb6121e4640b3045896c
mikeyhc <mikeyhc@atmosia.net>
2018-07-27 11:47:19 +0200

    move to windows

--- pal.c
+++ pal.c
@@ -14,6 +14,9 @@
 #define MAX_ENTRIES 10
 #define ENTRY_LENGTH 30
 
+#define MAX_WINDOWS 9
+
+
 typedef struct {
 	int max, current;
 } Bar;
@@ -23,6 +26,11 @@ typedef struct {
 	char status[16];
 	Bar hull, energy;
 } Ship;
+
+typedef struct {
+	Ship ship;
+	WINDOW *window;
+} ShipInfoRender;
 
 typedef struct {
 	unsigned debt_total;
@@ -53,90 +61,102 @@ typedef struct {
 	int x, y;
 } TableBuilder;
 
-void render_bar(char *name, Bar *bar)
+typedef struct {
+	TableBuilder table;
+	WINDOW *window;
+} TableRender;
+
+void render_bar(char *name, WINDOW *w, Bar *bar)
 {
 	int i;
 
-	printw("%7s [", name);
-	attron(A_REVERSE);
+	wprintw(w, "%7s [", name);
+	wattron(w, A_REVERSE);
 	for (i = 0; i < bar->current * 2; i++)
-		addch(' ');
-	attroff(A_REVERSE);
+		waddch(w, ' ');
+	wattroff(w, A_REVERSE);
 	for (i = 0; i < (bar->max- bar->current) * 2; i++)
-		addch(' ');
-	printw("] (%d/%d)", bar->current, bar->max);
+		waddch(w, ' ');
+	wprintw(w, "] (%d/%d)", bar->current, bar->max);
 }
 
 void
-ship_name_render(Ship *ship)
+ship_name_render(WINDOW *w, Ship *ship)
 {
 	if (ship->name[0] == '\0' || ship->status[0] == '\0')
 		return;
-	mvprintw(0, 0, "%s  [%s]", ship->name, ship->status);
+	mvwprintw(w, 0, 0, "%s  [%s]", ship->name, ship->status);
 }
 
 int
 ship_info_callback(void *vship, int cols,  char **vals, char **names)
 {
 	int v;
-	Ship *ship = vship;
+	ShipInfoRender *ship_render = vship;
+	WINDOW *w;
 	Bar b;
 
+	w = ship_render->window;
 	if (!strcmp(vals[0], "ship.hull")) {
 		v = atoi(vals[1]);
-		ship->hull.max = v;
-		if (ship->hull.max >= 0 && ship->hull.current >= 0) {
-			move(2, 0);
-			render_bar("Hull", &ship->hull);
+		ship_render->ship.hull.max = v;
+		if (ship_render->ship.hull.max >= 0 &&
+				ship_render->ship.hull.current >= 0) {
+			wmove(w, 2, 0);
+			render_bar("Hull", w, &ship_render->ship.hull);
 		}
 	} else if (!strcmp(vals[0], "ship.current_hull")) {
 		v = atoi(vals[1]);
-		ship->hull.current = v;
-		if (ship->hull.max >= 0 && ship->hull.current >= 0) {
-			move(2, 0);
-			render_bar("Hull", &ship->hull);
+		ship_render->ship.hull.current = v;
+		if (ship_render->ship.hull.max >= 0 &&
+				ship_render->ship.hull.current >= 0) {
+			wmove(w, 2, 0);
+			render_bar("Hull", w, &ship_render->ship.hull);
 		}
 	} else if (!strcmp(vals[0], "ship.energy")) {
 		v = atoi(vals[1]);
-		ship->energy.max = v;
-		if (ship->energy.max >= 0 && ship->energy.current >= 0) {
-			move(3, 0);
-			render_bar("Hull", &ship->energy);
+		ship_render->ship.energy.max = v;
+		if (ship_render->ship.energy.max >= 0 &&
+				ship_render->ship.energy.current >= 0) {
+			wmove(w, 3, 0);
+			render_bar("Hull", w, &ship_render->ship.energy);
 		}
 	} else if (!strcmp(vals[0], "ship.current_energy")) {
 		v = atoi(vals[1]);
-		ship->energy.current = v;
-		if (ship->energy.max >= 0 && &ship->energy.current >= 0) {
-			move(3, 0);
-			render_bar("Hull", &ship->energy);
+		ship_render->ship.energy.current = v;
+		if (ship_render->ship.energy.max >= 0 &&
+				&ship_render->ship.energy.current >= 0) {
+			wmove(w, 3, 0);
+			render_bar("Hull", w, &ship_render->ship.energy);
 		}
 	} else if (!strcmp(vals[0], "ship.armor")) {
-		move(4, 0);
+		wmove(w, 4, 0);
 		v = atoi(vals[1]);
 		b.current = b.max = v;
-		render_bar("Armor", &b);
+		render_bar("Armor", w, &b);
 	} else if (!strcmp(vals[0], "ship.name")) {
-		strncpy(ship->name, vals[1], 29);
-		ship_name_render(ship);
+		strncpy(ship_render->ship.name, vals[1], 29);
+		ship_name_render(w, &ship_render->ship);
 	} else if (!strcmp(vals[0], "ship.status")) {
-		strncpy(ship->status, vals[1], 15);
-		ship_name_render(ship);
+		strncpy(ship_render->ship.status, vals[1], 15);
+		ship_name_render(w, &ship_render->ship);
 	}
 	return 0;
 }
 
 void
-ship_info(sqlite3 *db)
+ship_info(sqlite3 *db, WINDOW *window)
 {
 	int i;
-	Ship ship;
+	ShipInfoRender ship_render;
 
-	ship.name[0] = '\0';
-	ship.status[0] = '\0';
-	ship.hull.max = ship.hull.current = -1;
-	ship.energy.max = ship.energy.current = -1;
+	ship_render.window = window;
+	ship_render.ship.name[0] = '\0';
+	ship_render.ship.status[0] = '\0';
+	ship_render.ship.hull.max = ship_render.ship.hull.current = -1;
+	ship_render.ship.energy.max = ship_render.ship.energy.current = -1;
 	sqlite3_exec(db, "SELECT * FROM properties WHERE name LIKE 'ship.%'",
-			&ship_info_callback, &ship, NULL);
+			&ship_info_callback, &ship_render, NULL);
 }
 
 int
@@ -162,10 +182,10 @@ cost_info_callback(void *vcosts, int cols, char **vals, char **names)
 }
 
 void
-cost_info(sqlite3 *db)
+cost_info(sqlite3 *db, WINDOW *w, WINDOW *bw)
 {
 	Costs costs;
-	int repayment_percent;
+	int repayment_percent, lines;
 	float repayment_points;
 	char *msg = "Repaid";
 
@@ -173,104 +193,118 @@ cost_info(sqlite3 *db)
 			"OR name LIKE 'overhaul.%'", &cost_info_callback,
 			&costs, NULL);
 
-	mvprintw(1, OFFSET, "Ship Costs:");
-	mvprintw(2, OFFSET + 2, "Debt:");
-	mvprintw(3, OFFSET + 4, "Total:  %10d", costs.debt_total);
+	mvwprintw(w, 0, 0, "Ship Costs:");
+	mvwprintw(w, 1, 2, "Debt:");
+	mvwprintw(w, 2, 4, "Total:  %10d", costs.debt_total);
 	repayment_percent = costs.debt_repaid * 100 / costs.debt_total;
-	mvprintw(4, OFFSET + 4, "Repaid: %10d (%3d%)", costs.debt_repaid,
+	repayment_percent = 25;
+	mvwprintw(w, 3, 4, "Repaid: %10d (%3d%)", costs.debt_repaid,
 			repayment_percent);
-	mvprintw(5, OFFSET + 4, "Repayments:");
-	mvprintw(6, OFFSET + 6, "Per Cycle:   %8.0f",
+	mvwprintw(w, 4,  4, "Repayments:");
+	mvwprintw(w, 5,  6, "Per Cycle:   %8.0f",
 			round(costs.debt_total / 20.0));
-	mvprintw(7, OFFSET + 6, "Per Segment: %8.0f",
+	mvwprintw(w, 6, 6, "Per Segment: %8.0f",
 			round(costs.debt_total / 180.0));
-	mvprintw(8, OFFSET + 2, "Servicing:");
-	mvprintw(9, OFFSET + 4, "Frontier: %13d", costs.overhaul_frontier);
-	mvprintw(10, OFFSET + 4, "Standard: %13d", costs.overhaul_standard);
-	mvprintw(11, OFFSET + 4, "Advanced: %13d", costs.overhaul_advanced);
+	mvwprintw(w, 7, 2, "Servicing:");
+	mvwprintw(w, 8, 4, "Frontier: %13d", costs.overhaul_frontier);
+	mvwprintw(w, 9, 4, "Standard: %13d", costs.overhaul_standard);
+	mvwprintw(w, 10, 4, "Advanced: %13d", costs.overhaul_advanced);
 
-	repayment_points = repayment_percent / (100.0 / (LINES - 3));
-	mvprintw(LINES - 3, COLS - strlen(msg), msg);
-	attron(A_REVERSE);
-	mvvline(LINES - 3 - repayment_points, COLS - 1, ' ',
+	lines = getmaxy(bw);
+	repayment_points = repayment_percent / (100.0 / lines);
+	mvwprintw(bw, lines - 1, 0, msg);
+	wattron(bw, A_REVERSE);
+	mvwvline(bw, lines - repayment_points, COLS - 1, ' ',
 			(int)round(repayment_points));
-	attroff(A_REVERSE);
+	wattroff(bw, A_REVERSE);
+	wrefresh(bw);
 }
 
 int
-crew_info_callback(void *unused, int cols, char **vals, char **names)
+crew_info_callback(void *vw, int cols, char **vals, char **names)
 {
 	int y;
+	WINDOW *w = vw;
 
-	printw("%22s", vals[0]);
-	y = getcury(stdscr);
-	mvprintw(y, 24, "[%s]\n", vals[1]);
+	y = getcury(w);
+	mvwprintw(w, y + 1, 0, "%22s", vals[0]);
+	mvwprintw(w, y + 1, 24, "[%s]", vals[1]);
 
 }
 
 void
-crew_info(sqlite3 *db)
+crew_info(sqlite3 *db, WINDOW *w)
 {
-	int i;
-
-	mvprintw(6, 0, "Crew Status:\n");
-	sqlite3_exec(db, "SELECT * FROM crew", &crew_info_callback, NULL,
+	mvwprintw(w, 0, 0, "Crew Status:\n");
+	sqlite3_exec(db, "SELECT * FROM crew", &crew_info_callback, w,
 			NULL);
 }
 
 int
-module_info_callback(void *vbuilder, int cols, char **vals, char **names)
+module_info_callback(void *vrender, int cols, char **vals, char **names)
 {
-	TableBuilder *builder = vbuilder;
+	TableRender *render = vrender;
+	WINDOW *w = render->window;
+	TableBuilder *builder = &render->table;
+
 	if (builder->counters[0] > 0 && builder->counters[0] % 3 == 0) {
 		builder->y++;
 		builder->x = 0;
 	}
 	if (!strcmp(vals[1], "EE"))
-		attron(COLOR_PAIR(1));
+		wattron(w, COLOR_PAIR(1));
 	else
 		builder->counters[1]++;
-	mvprintw(builder->y, builder->x, "%25s  [%s]", vals[0], vals[1]);
+	mvwprintw(w, builder->y, builder->x, "%25s  [%s]", vals[0], vals[1]);
 	builder->x += 31;
 	if (!strcmp(vals[1], "EE"))
-		attroff(COLOR_PAIR(1));
+		wattroff(w, COLOR_PAIR(1));
 	builder->counters[0]++;
 
 	return 0;
 }
 
 int
-module_info_header(void *vcounter, int cols, char **vals, char **names)
+module_info_header(void *vrender, int cols, char **vals, char **names)
 {
-	int *counters = vcounter;
+	TableRender *render = vrender;
+	int *counters = render->table.counters;
 	int max_modules = atoi(vals[0]);
 
-	mvprintw(13, 0, "Installed Modules (%d free, %d enabled):",
+	mvwprintw(render->window, 0, 0,
+			"Installed Modules (%d free, %d enabled):",
 			max_modules - *counters, counters[1]);
 	return 0;
 }
 
 void
-module_info(sqlite3 *db)
+module_info(sqlite3 *db, WINDOW *w)
 {
-	TableBuilder builder = { {0, 0}, 0, 14 };
+	TableRender render = {
+		{ {0, 0}, 0, 1 },
+		w
+	};
+
 
 	sqlite3_exec(db, "SELECT * FROM modules", &module_info_callback,
-			&builder, NULL);
+			&render, NULL);
 	sqlite3_exec(db, "SELECT value FROM properties "
 			"WHERE name = 'ship.max_modules'", &module_info_header,
-			&builder, NULL);
+			&render, NULL);
 }
 
 int
-feature_info_callback(void *vbuilder, int cols, char **vals, char **names)
+feature_info_callback(void *vrender, int cols, char **vals, char **names)
 {
-	TableBuilder *builder= vbuilder;
+	TableRender *render = vrender;
+	TableBuilder *builder = &render->table;
+
 	if (builder->counters[0] > 0 && builder->counters[0] % 3 == 0) {
 		builder->y++;
 		builder->x = 0;
 	}
-	mvprintw(builder->y, builder->x, "%25s  [%s]", vals[0], vals[1]);
+	mvwprintw(render->window, builder->y, builder->x,
+			"%25s  [%s]", vals[0], vals[1]);
 	builder->x += 36;
 	builder->counters[0]++;
 
@@ -278,24 +312,30 @@ feature_info_callback(void *vbuilder, int cols, char **vals, char **names)
 }
 
 void
-feature_info(sqlite3 *db)
+feature_info(sqlite3 *db, WINDOW *w)
 {
-	TableBuilder builder = { {0}, 0, 20 };
+	TableRender render = {
+		{ {0}, 0, 1 },
+		w
+	};
 
-	mvprintw(19, 0, "Installed Features:");
+	mvwprintw(w, 0, 0, "Installed Features:");
 	sqlite3_exec(db, "SELECT * FROM features", &feature_info_callback,
-			&builder, NULL);
+			&render, NULL);
 }
 
 int
-cargo_info_callback(void *vbuilder, int cols, char **vals, char **names)
+cargo_info_callback(void *vrender, int cols, char **vals, char **names)
 {
-	TableBuilder *builder = vbuilder;
+	TableRender *render = vrender;
+	TableBuilder *builder = &render->table;
+
 	if (builder->counters[0] > 0 && builder->counters[0] % 3 == 0) {
 		builder->y++;
 		builder->x = 0;
 	}
-	mvprintw(builder->y, builder->x, "%25s  [%2s]", vals[0], vals[1]);
+	mvwprintw(render->window, builder->y, builder->x,
+			"%25s  [%2s]", vals[0], vals[1]);
 	builder->x += 36;
 	builder->counters[0]++;
 
@@ -303,117 +343,118 @@ cargo_info_callback(void *vbuilder, int cols, char **vals, char **names)
 }
 
 void
-cargo_info(sqlite3 *db)
+cargo_info(sqlite3 *db, WINDOW *w)
 {
-	TableBuilder builder = { {0}, 0, 23 };
+	TableRender render = {
+		{ {0}, 0, 1 },
+		w,
+	};
 
-	mvprintw(22, 0, "Cargo Contents:");
+	mvwprintw(w, 0, 0, "Cargo Contents:");
 	sqlite3_exec(db, "SELECT * FROM cargo", &cargo_info_callback,
-			&builder, NULL);
+			&render, NULL);
 }
 
 void
-error_info()
+error_info(WINDOW *w)
 {
 	int i;
 	char *msg = "+++ ERROR +++ I have accidently seen spoilers for "
 		"the next episode ;-; +++ ERROR +++";
-	attron(A_REVERSE);
-	mvprintw(LINES - 2, 0, msg);
-	// TODO: this is terrible, think of something better
-	for (i = strlen(msg); i < COLS; i++)
-		addch(' ');
-	attron(A_REVERSE);
+	wattron(w, A_REVERSE);
+	mvwprintw(w, 0, 0, msg);
+	whline(w, ' ', COLS - strlen(msg));
+	wattron(w, A_REVERSE);
 }
 
 void
-render_menu_selected(int y, int x, int i, MenuInfo *menuinfo)
+render_menu_selected(WINDOW *w, int y, int x, int i, MenuInfo *menuinfo)
 {
 	if (menuinfo->mode == MM_EDIT) {
-		mvprintw(y, x, menuinfo->entries[i]);
-		attron(A_REVERSE);
-		addch(' ');
-		attroff(A_REVERSE);
+		mvwprintw(w, y, x, menuinfo->entries[i]);
+		wattron(w, A_REVERSE);
+		waddch(w,' ');
+		wattroff(w, A_REVERSE);
 	} else {
-		attron(A_REVERSE);
-		mvprintw(y, x, menuinfo->entries[i]);
-		hline(' ', ENTRY_LENGTH - strlen(menuinfo->entries[i]));
-		attroff(A_REVERSE);
+		wattron(w, A_REVERSE);
+		mvwprintw(w, y, x, menuinfo->entries[i]);
+		whline(w,' ', ENTRY_LENGTH - strlen(menuinfo->entries[i]));
+		wattroff(w, A_REVERSE);
 	}
 }
 
 void
-render_menu(int y, int x, MenuInfo *menuinfo)
+render_menu(WINDOW *w, int y, int x, MenuInfo *menuinfo)
 {
 	int i;
 
 	for (i = 0; i < MAX_ENTRIES; i++) {
 		if (i == menuinfo->curline)
-			render_menu_selected(y + i, x, i, menuinfo);
+			render_menu_selected(w, y + i, x, i, menuinfo);
 		else
-			mvprintw(y + i, x, menuinfo->entries[i]);
+			mvwprintw(w, y + i, x, menuinfo->entries[i]);
 	}
 }
 
 void
-battle_info(MenuInfo *menuinfo)
+battle_info(MenuInfo *menuinfo, WINDOW *w)
 {
-	mvprintw(1, OFFSET, "Combat Order:");
-	render_menu(2, OFFSET + 2, menuinfo);
+	mvwprintw(w, 0, 0, "Combat Order:");
+	render_menu(w, 1, 2, menuinfo);
 }
 
 void
-load_default_ui(sqlite3 *db)
+load_default_ui(sqlite3 *db, WINDOW **windows)
 {
 	erase();
 	move(0, 0);
-	ship_info(db);
-	crew_info(db);
-	cost_info(db);
-	module_info(db);
-	feature_info(db);
-	cargo_info(db);
-	error_info();
+	ship_info(db, windows[0]);
+	crew_info(db, windows[1]);
+	cost_info(db, windows[2], windows[8]);
+	module_info(db, windows[3]);
+	feature_info(db, windows[4]);
+	cargo_info(db, windows[5]);
+	error_info(windows[6]);
 }
 
 void
-load_battle_ui(sqlite3 *db, MenuInfo *menuinfo)
+load_battle_ui(sqlite3 *db, WINDOW **windows, MenuInfo *menuinfo)
 {
 	erase();
 	move(0, 0);
-	ship_info(db);
-	crew_info(db);
-	battle_info(menuinfo);
-	module_info(db);
-	feature_info(db);
-	cargo_info(db);
-	error_info();
+	ship_info(db, windows[0]);
+	crew_info(db, windows[1]);
+	battle_info(menuinfo, windows[2]);
+	module_info(db, windows[3]);
+	feature_info(db, windows[4]);
+	cargo_info(db, windows[5]);
+	error_info(windows[6]);
 }
 
 void
-load_nav_ui(UIType ui)
+load_nav_ui(UIType ui, WINDOW *w)
 {
-	move(LINES - 1, 0);
-	attron(A_REVERSE);
-	printw(" F1 ");
+	wmove(w, 0, 0);
+	wattron(w, A_REVERSE);
+	wprintw(w, " F1 ");
 	if (ui != UI_DEFAULT)
-		attroff(A_REVERSE);
-	printw(" Ship ");
+		wattroff(w, A_REVERSE);
+	wprintw(w, " Ship ");
 	if (ui == UI_DEFAULT)
-		attroff(A_REVERSE);
-	addch(' ');
-	attron(A_REVERSE);
-	printw(" F2 ");
+		wattroff(w, A_REVERSE);
+	waddch(w, ' ');
+	wattron(w, A_REVERSE);
+	wprintw(w, " F2 ");
 	if (ui != UI_BATTLE)
-		attroff(A_REVERSE);
-	printw(" Battle ");
+		wattroff(w, A_REVERSE);
+	wprintw(w, " Battle ");
 	if (ui == UI_BATTLE)
-		attroff(A_REVERSE);
-	addch(' ');
-	attron(A_REVERSE);
-	printw(" F10 ");
-	attroff(A_REVERSE);
-	printw(" Quit ");
+		wattroff(w, A_REVERSE);
+	waddch(w, ' ');
+	wattron(w, A_REVERSE);
+	wprintw(w, " F10 ");
+	wattroff(w, A_REVERSE);
+	wprintw(w, " Quit ");
 }
 
 void
@@ -507,8 +548,20 @@ main(int argc, char **argv)
 	struct pollfd ufds[1];
 	MenuInfo menuinfo;
 	int running = 1, ready;
-	int c, failcount = 0;
+	int i, c, failcount = 0;
 	UIType ui = UI_DEFAULT;
+	int window_positions[MAX_WINDOWS][4] = {
+		{ 5, 40, 0, 0 },
+		{ 8, 40, 7, 0 },
+		{ 20, 40, 0, OFFSET },
+		{ 6, 120, 15, 0 },
+		{ 3, 120, 21, 0 },
+		{ 4, 120, 24, 0 },
+		{ 1, -1, -3, 0 },
+		{ 1, -1, -2, 0 },
+		{ -3, 7, 0, -8},
+	};
+	WINDOW *windows[MAX_WINDOWS];
 
 	setlocale(LC_ALL, "");
 	assert(!sqlite3_open("pal.db", &db));
@@ -523,10 +576,28 @@ main(int argc, char **argv)
 	start_color();
 	init_pair(1, COLOR_RED, COLOR_BLACK);
 	keypad(stdscr, 1);
+	curs_set(0);
 
-	load_default_ui(db);
-	load_nav_ui(ui);
-	refresh();
+	for (i = 0; i < MAX_WINDOWS; i++) {
+		if (window_positions[i][0] < 0)
+			window_positions[i][0] = LINES + 1 +
+				window_positions[i][0];
+		if (window_positions[i][1] < 0)
+			window_positions[i][1] = COLS + 1 +
+				window_positions[i][1];
+		if (window_positions[i][2] < 0)
+			window_positions[i][2] = LINES + 1 +
+				window_positions[i][2];
+		if (window_positions[i][3] < 0)
+			window_positions[i][3] = COLS + 1 +
+				window_positions[i][3];
+		windows[i] = newwin(
+				window_positions[i][0],
+				window_positions[i][1],
+				window_positions[i][2],
+				window_positions[i][3]);
+	}
+
 	while (1) {
 		if (poll(ufds, 1, TIMEOUT) < 0) {
 			if (failcount == 5)
@@ -555,14 +626,15 @@ main(int argc, char **argv)
 
 		switch (ui) {
 		case UI_DEFAULT:
-			load_default_ui(db);
+			load_default_ui(db, windows);
 			break;
 		case UI_BATTLE:
-			load_battle_ui(db, &menuinfo);
+			load_battle_ui(db, windows, &menuinfo);
 			break;
 		}
-		load_nav_ui(ui);
-		refresh();
+		load_nav_ui(ui, windows[7]);
+		for (i = 0; i < MAX_WINDOWS; i++)
+			wrefresh(windows[i]);
 	}
 
 END:
